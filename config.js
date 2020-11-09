69aa46c2aa8d8cf62a6dbd297d95f6d1418f931a
kevintjin <57367379+kevintjin@users.noreply.github.com>
2020-11-09 14:01:16 +0800

    Update config.js

--- config.js
+++ config.js
@@ -31,8 +31,8 @@ var config = {
     desc: {
         turn_on: "开始",
         play: "音乐",
-        bannar_coming: "颜色",
-        balloons_flying: "好像少点东西",
+        bannar_coming: "横幅来咯",
+        balloons_flying: "好像还少点东西",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
         wish_message: "生日快乐",
